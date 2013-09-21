0d85db4bc40f940cf9f6316116255197b3d7bc80
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-09-21 12:21:31 -0700

    Initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hashiru::Application.config.secret_key_base = '59c75f88e9d82fbf146950b64eff57af9593abd81801afa5709b61df25c7a39e8236aed18bdae5957662408294e13f6b7c09436055e08d0b9b51d77c4a5b5081'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hashiru::Application.config.session_store :cookie_store, key: '_hashiru_session'
