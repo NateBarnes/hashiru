d1f548bdd81e29c4cefc215d49ff3533d7ae8362
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-09-21 14:06:57 -0700

    twitter bootstrap and initial views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    # if logged in
+    #   redirect to choose
+    # else
+    #   redirect to register
   end
+
+  def register
+    
+  end
+
+  def choose
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Hashiru::Application.routes.draw do
   root 'home#index'
+
+  get  'register' => 'home#register'
+  post 'register' => 'home#register'
 end
