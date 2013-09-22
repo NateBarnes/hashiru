00c98ec606b0a38e19c1a6bc43034650bd0de9cf
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-09-21 21:46:34 -0700

    adds event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130922011918) do
+ActiveRecord::Schema.define(version: 20130922044545) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.datetime "date"
+    t.string   "distance"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "exercises", force: true do |t|
     t.integer  "time"
