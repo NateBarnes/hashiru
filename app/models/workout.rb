c8e777517d380447b2162963a9847eaaa744a8d1
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-09-22 01:07:48 -0700

    sidekiq updates

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,5 +3,11 @@ class Workout < ActiveRecord::Base
   has_many :workout_exercises, :dependent => :destroy
   has_many :exercises, :through => :workout_exercises
   has_many :measurements, :through => :workout_exercises
+
+  after_save :repopulate_efficacy
+
+  def repopulate_efficacy
+    GeneralEfficacyGenerator.perform_async
+  end
 end
 
