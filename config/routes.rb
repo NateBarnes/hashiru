abf70bc297c678e66559e58dd314d79a3b6f7857
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-09-22 05:02:16 -0700

    Skeleton of UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Hashiru::Application.routes.draw do
   root 'home#today'
 
   get 'today' => 'home#today', as: :today
+  get 'search' => 'home#search', as: :search
 
   resources :users
   resources :workouts
