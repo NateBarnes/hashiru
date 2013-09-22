8269ba2dabf253f266fdf65876234858d2d2fdda
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-09-22 09:10:23 -0700

    adds logout link

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Hashiru::Application.routes.draw do
   get 'today' => 'home#today', as: :today
   get 'search' => 'home#search', as: :search
   post 'save_event' => 'home#save_event', as: :save_event
+  get 'logout' => 'home#logout', as: :logout
 
   resources :users
   resources :workouts
