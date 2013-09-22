181f0518b7ec03a91b1a80e4c9895545ebd2dca7
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-09-21 21:52:54 -0700

    General Efficacy DONE BITCHES

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,9 @@
 class Exercise < ActiveRecord::Base
   has_many :workout_exercises, :dependent => :destroy
   has_many :workouts, :through => :workout_exercises
+  has_one :general_efficacy, :dependent => :destroy
+
+  after_create :create_efficacies
 
   def self.distance_run
     Exercise.find_by_name "Long Slow Run"
@@ -8,5 +11,9 @@ class Exercise < ActiveRecord::Base
 
   def self.speed_run
     Exercise.find_by_name "Fast 1 Mile"
+  end
+
+  def create_efficacies
+    GeneralEfficacy.create :exercise_id => self.id, :speed_score => 0, :distance_score => 0, :general_score => 0
   end
 end
